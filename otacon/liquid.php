b536af45cc33b873bc542253d14f9f0c623d3dcd
a31nesta <anthroponesta@gmail.com>
2024-10-02 12:15:47 +0200

    initial commit

---
+++ otacon/liquid.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="../imgs/favicon.ico">
+
+    <title>Big Boss Fanclub</title>
+
+    <link rel="stylesheet" href="../Styles/mainStyle.css">
+    <link rel="stylesheet" href="../Styles/carStyles.css">
+</head>
+<body>
+    <div class="content-div">
+        <h1>Metal Gear Liquid<br></h1>
+
+        <!-- BIG BOSS -->
+        <embed src="../imgs/when you cant even say.svg">
+
+        <div class="flexicinta">
+            <div class="division">
+                <!-- Why are we still here? -->
+                <p>
+                    Why are we still here? Just to suffer? Every night, I can feel my arm... and my leg... even my fingers.
+                    The body I've lost... The comrades I've lost... won't stop hurting.
+                    It’s like they’re all still there. You feel it, too, don’t you?
+                </p>
+
+                <?php
+                    echo "<a href=https://www.youtube.com/watch?v=dQw4w9WgXcQ><br>Hola Mundo</a>";
+
+                    // Start p
+                    echo "<p>";
+
+                    $var = 15;
+                    echo $var;
+                    $var = "<br>Hello gays";
+                    echo $var;
+
+                    echo "</p>";
+                ?>
+                <a href="https://youtu.be/LGBva8ZylCc" alt="memes" target="_blank">
+                <div class="card">
+                <div class="wrapper">
+                <img src="../imgs/box.png" class="cover-image" />
+                </div>
+                <img src="../imgs/title.png" class="title" />
+                <img src="../imgs/drip.png" class="character" />
+                </div>
+                </a>
+                
+            </div>
+            <div class="division">
+                <section class="slider_container">
+                    <section class="slider">
+                        <div class="slide one">
+                            <img src="../imgs/TPPBigBossPP.webp" alt="Kept you waiting, huh?">
+                        </div>
+                        <div class="slide two">
+                            <img src="../imgs/TSOtaconPP.webp" alt="It's like one of my japanese animes!">
+                        </div>
+                    </section>
+                </section>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
